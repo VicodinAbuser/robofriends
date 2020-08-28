4777f4fb3cc3ac105eb894deb7a24116c74efc1d
Naman Agarwal <thenamanagarwal@gmail.com>
2020-08-29 01:06:52 +0530

    Added size query parameter to the robohash API

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card(props) {
     const { name, email, id } = props;
     return (
         <div className='bg-light-blue tc dib br3 ma3 pa2 grow bw-2 shadow-5'>
-            <img src={`https://robohash.org/${id}`} alt='robotpic'/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt='robotpic'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
